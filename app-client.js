66db4d49d3aff9acf96edbdc49d6071bdc96f7c6
Jared Cain <jared.cain77@gmail.com>
2016-05-28 14:57:56 -0700

    Update node version in package.json

--- app-client.js
+++ app-client.js
@@ -8,7 +8,7 @@ require('./js/avatarSection');
 var SnackBar = require('node-snackbar/dist/snackbar');
 
 // WOW cloned from 'amd' branch of wowjs
-var WOW = require('./node_modules/WOW/dist/wow.js');
+var WOW = require('./node_modules/WOW/dist/wow');
 
 $(document).ready(function(){
     $('a.page-scroll').bind('click', function(event) {
